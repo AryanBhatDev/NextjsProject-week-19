3b0681d3c5fb4fc572b3741015767d64e9d1dd94
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-21 19:51:26 +0530

    2nd lec completed

--- app/api/v1/user/signup/route.ts
+++ app/api/v1/user/signup/route.ts
@@ -1,11 +1,19 @@
+import prisma from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+
 
 export async function POST(req:NextRequest){
     const data = await req.json();
-    
-    console.log(data);
 
+    const user = await prisma.user.create({
+        data:{
+            email:data.email,
+            password: data.password
+        }
+    })
+    console.log(user);
     return NextResponse.json({
-        msg:"Signed up"
+        msg:"Signed up",
+        username: user.email
     })
 }
