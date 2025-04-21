acce3e494252e86610503d0a024f0e9661da60b6
Aryan Bhat <aryanbhat2527@gmail.com>
2025-04-21 17:29:58 +0530

    created basic fe ui and be apis

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import axios from "axios";
+import { useState } from "react";
+
+export default function Signup() {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    return (
+        <div className='h-screen w-screen flex items-center flex-col  justify-center'>
+            <input
+                className='border border-white p-2 m-2'
+                type='text'
+                onChange={(e)=>{
+                    setEmail(e.target.value)
+                }}
+                placeholder='Email'
+            ></input>
+            <input
+                className='border border-white p-2 m-2'
+                type='text'
+                onChange={(e)=>{
+                    setPassword(e.target.value)
+                }}
+                placeholder='Password'
+            ></input>
+            <button onClick={()=>{
+                axios.post("http://localhost:3000/api/v1/user/signup",{
+                    email,
+                    password
+                })
+            }} className="rounded-md border border-white bg-blue-500 mt-2 w-40 h-10">
+                Signup
+            </button>
+        </div>
+    );
+}
